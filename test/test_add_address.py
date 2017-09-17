61d4db37230b24c509f70828e944ddce9aa7652d
PaulRumyantsev <chrome8282@gmail.com>
2017-09-17 16:11:48 +0300

    contacts class creation + refactor

--- test/test_add_address.py
+++ test/test_add_address.py
@@ -1,9 +1,5 @@
 # -*- coding: utf-8 -*-
-import unittest
-
 import pytest
-from selenium.webdriver.firefox.webdriver import WebDriver
-
 from fixture.application import Application
 from model.address import Address
 
@@ -15,89 +11,16 @@ def app(request):
     return fixture
 
 
-class test_add_address(unittest.TestCase):
-    def setUp(self):
-        self.wd = WebDriver(capabilities={"marionette": False})
-        self.wd.implicitly_wait(60)
-    
-    def test_add_address(self):
-        self.login(username="admin", password="secret")
-        self.create_address(Address(firstname="test", middlename="test", lastname="test", title="test", nickname="test", company="test", home="test", email="test", address="test"))
-        self.logout()
+def test_add_address(app):
+        app.session.login(username="admin", password="secret")
+        app.contacts.create(Address(firstname="test", middlename="test", lastname="test", title="test", nickname="test", company="test", home="test", email="test", address="test"))
+        app.session.logout()
 
-    def test_add_empty_address(self):
-        self.login(username="admin", password="secret")
-        self.create_address(Address(firstname="", middlename="", lastname="", title="", nickname="", company="", home="", email="", address=""))
-        self.logout()
 
-    def logout(self):
-        wd = self.wd
-        wd.find_element_by_link_text("Logout").click()
-        wd.find_element_by_id("LoginForm").click()
+def test_add_empty_address(app):
+        app.session.login(username="admin", password="secret")
+        app.contacts.create(Address(firstname="", middlename="", lastname="", title="", nickname="", company="", home="", email="", address=""))
+        app.session.logout()
 
-    def return_to_home_page(self):
-        wd = self.wd
-        wd.find_element_by_link_text("home page").click()
 
-    def create_address(self, address):
-        wd = self.wd
-        # address creation
-        wd.find_element_by_link_text("add new").click()
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(address.firstname)
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("middlename").click()
-        wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(address.middlename)
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(address.lastname)
-        wd.find_element_by_name("address").click()
-        wd.find_element_by_name("title").click()
-        wd.find_element_by_name("title").clear()
-        wd.find_element_by_name("title").send_keys(address.title)
-        wd.find_element_by_name("nickname").click()
-        wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(address.nickname)
-        wd.find_element_by_name("address").click()
-        wd.find_element_by_name("company").click()
-        wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys(address.company)
-        wd.find_element_by_name("address").click()
-        wd.find_element_by_name("home").click()
-        wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys(address.home)
-        wd.find_element_by_name("theform").click()
-        wd.find_element_by_name("email").click()
-        wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(address.email)
-        wd.find_element_by_name("email3").click()
-        wd.find_element_by_name("address2").click()
-        wd.find_element_by_name("address2").clear()
-        wd.find_element_by_name("address2").send_keys(address.address)
-        # submit address creation
-        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
-        self.return_to_home_page()
 
-    def login(self, username, password):
-        wd = self.wd
-        self.open_home_page()
-        # login
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
-
-    def open_home_page(self):
-        wd = self.wd
-        wd.get("http://localhost/addressbook/index.php")
-
-    def tearDown(self):
-        self.wd.quit()
-
-if __name__ == '__main__':
-    unittest.main()
