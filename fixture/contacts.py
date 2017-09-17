8a43f0d283b07a6b985e9d75bf07191c95269183
PaulRumyantsev <chrome8282@gmail.com>
2017-09-17 16:47:34 +0300

    group deletion test creation

---
+++ fixture/contacts.py
@@ -0,0 +1,51 @@
+
+
+class ContactHelper:
+    def __init__(self, app):
+        self.app = app
+
+    def return_to_home_page(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("home page").click()
+
+    def create(self, address):
+        wd = self.app.wd
+        # address creation
+        wd.find_element_by_link_text("add new").click()
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(address.firstname)
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("middlename").click()
+        wd.find_element_by_name("middlename").clear()
+        wd.find_element_by_name("middlename").send_keys(address.middlename)
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(address.lastname)
+        wd.find_element_by_name("address").click()
+        wd.find_element_by_name("title").click()
+        wd.find_element_by_name("title").clear()
+        wd.find_element_by_name("title").send_keys(address.title)
+        wd.find_element_by_name("nickname").click()
+        wd.find_element_by_name("nickname").clear()
+        wd.find_element_by_name("nickname").send_keys(address.nickname)
+        wd.find_element_by_name("address").click()
+        wd.find_element_by_name("company").click()
+        wd.find_element_by_name("company").clear()
+        wd.find_element_by_name("company").send_keys(address.company)
+        wd.find_element_by_name("address").click()
+        wd.find_element_by_name("home").click()
+        wd.find_element_by_name("home").clear()
+        wd.find_element_by_name("home").send_keys(address.home)
+        wd.find_element_by_name("theform").click()
+        wd.find_element_by_name("email").click()
+        wd.find_element_by_name("email").clear()
+        wd.find_element_by_name("email").send_keys(address.email)
+        wd.find_element_by_name("email3").click()
+        wd.find_element_by_name("address2").click()
+        wd.find_element_by_name("address2").clear()
+        wd.find_element_by_name("address2").send_keys(address.address)
+        # submit address creation
+        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
+        self.return_to_home_page()
+
