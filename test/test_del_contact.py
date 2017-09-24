3ec4d04f5996b0ccb7a4f5d16c99510ff96043ed
PaulRumyantsev <chrome8282@gmail.com>
2017-09-24 14:05:15 +0300

    preoptimization

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -2,4 +2,6 @@ from model.address import Address
 
 
 def test_delete_first_contact(app):
+    if app.contacts.count() == 0:
+        app.contacts.create(Address(firstname="test"))
     app.contacts.delete_first_contact()
--- test/test_modif_contact.py
+++ test/test_modif_contact.py
@@ -2,5 +2,7 @@ from model.address import Address
 
 
 def test_modif_first_contact(app):
-    app.contacts.modif_first_contact(Address(firstname="New firstname"))
+    if app.contacts.count() == 0:
+        app.contacts.create(Address(firstname="test"))
+    app.contacts.modif_first_contact(Address(firstname="test1"))
 
