9c2d6b3a55e8c29ac271ea48066140471ae05c12
PaulRumyantsev <chrome8282@gmail.com>
2017-10-22 01:37:30 +0300

    lesson 7 db fixture and test created

--- check_db_connection.py
+++ check_db_connection.py
@@ -4,7 +4,7 @@ connection = mysql.connector.connect(host="127.0.0.1", database="addressbook", u
 
 try:
     cursor = connection.cursor()
-    cursor.execute("select * from group_list")
+    cursor.execute("select * from addressbook")
     for row in cursor.fetchall():
         print(row)
 finally:
