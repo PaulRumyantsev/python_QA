0d8bea2ac962508d73a099467bcf9f8231a7d1e7
PaulRumyantsev <chrome8282@gmail.com>
2017-11-12 22:53:17 +0200

    Allure add contact

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -5,12 +5,15 @@ import pytest
 
 def test_add_contact(app, db, json_contacts):
     contacts = json_contacts
-    old_contacts = db.get_contacts_list()
-    app.contacts.create(contacts)
-    assert len(old_contacts) + 1 == app.contacts.count()
-    new_contacts = db.get_contacts_list()
-    old_contacts.append(contacts)
-    assert sorted(old_contacts, key=Contacts.id_or_max) == sorted(new_contacts, key=Contacts.id_or_max)
+    with pytest.allure.step('Given a contact list'):
+        old_contacts = db.get_contacts_list()
+    with pytest.allure.step('When I add the contact to the list'):
+        app.contacts.create(contacts)
+    with pytest.allure.step('Then the new contact list is equal to the old list with the added contact'):
+        assert len(old_contacts) + 1 == app.contacts.count()
+        new_contacts = db.get_contacts_list()
+        old_contacts.append(contacts)
+        assert sorted(old_contacts, key=Contacts.id_or_max) == sorted(new_contacts, key=Contacts.id_or_max)
 
 
 
